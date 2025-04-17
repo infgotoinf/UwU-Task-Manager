ab6f094b394c58b2a704bbe1fd8dfb4f0c250e3c
studentColl <ggxaoc@gmail.com>
2025-04-17 09:19:31 +0300

    python to rust progress

--- tauri-app2/src-tauri/src/lib.rs
+++ tauri-app2/src-tauri/src/lib.rs
@@ -1,27 +1,42 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::env;
-
-// fn main() -> std::io::Result<()> {
-//     let path = env::current_dir()?;
-//     println!("The current directory is {}", path.display());
-//     Ok(())
-// }
-
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
+use std::{env, ffi::{CStr, c_char}};
 
 // #[tauri::command]
 // fn my_custom_command() -> String {
 //     serde_json::to_string(&env::current_dir()).into()
 // }
 
+struct Process{
+    name: String,
+    pid: String,
+    mem_use: String,
+    cpu_use: String
+}
+
+impl Process
+{
+    fn new(name : &str, pid : &str, mem_use : &str, cpu_use : &str) -> Self
+    {
+        Self {name : name.to_owned(), pid : pid.to_owned(), mem_use : mem_use.to_owned(), cpu_use : cpu_use.to_owned()}
+    }
+}
+
 #[tauri::command]
 fn my_custom_command2() -> String {
-    let path = env::current_dir().unwrap();
-    format!("{}\\..\\..\\dll\\Dll1\\x64\\Debug\\Dll1.dll", path.display()).to_string()
+    unsafe {
+        let path = env::current_dir().unwrap();
+        let str_path = format!("{}\\..\\..\\dll\\Dll1\\x64\\Debug\\Dll1.dll", path.display()).to_string();
+        let lib = libloading::Library::new(str_path).unwrap();
+        let func: libloading::Symbol<unsafe extern "C" fn() -> *const c_char> = lib.get(b"PrintProcessInfo").expect("ErrorLOL");
+        let ptr_result = func();
+        let c_result = CStr::from_ptr(ptr_result);
+        c_result.to_string_lossy().into_owned()
+    }
 }
+// fn my_custom_command2() -> String {
+//     let path = env::current_dir().unwrap();
+//     format!("{}\\..\\..\\dll\\Dll1\\x64\\Debug\\Dll1.dll", path.display()).to_string()
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
