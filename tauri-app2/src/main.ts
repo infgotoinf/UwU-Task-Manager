39606586cde5b55737fc2135763f9570ccdcc512
idrk1618 <126374482+idrk1618@users.noreply.github.com>
2025-04-27 15:02:28 +0300

    yup

--- tauri-app2/src/main.ts
+++ tauri-app2/src/main.ts
@@ -1,33 +1,47 @@
 import { invoke } from "@tauri-apps/api/core";
 
-// let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+let table: HTMLElement | null = document.querySelector(".table");
+const selectedPIDs = new Set<string>(); 
 
 async function greet() {
-  if (greetMsgEl) { // (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    let da: Array<string> = await invoke("my_custom_command2");
-    let iter = 1;
-    greetMsgEl.innerHTML = ``;
-    for (const i in da) {
-      if (iter == 1) {
-        greetMsgEl.innerHTML += `<div>${da[i]} ${da[Number(i) + 1]}
-        ${da[Number(i) + 2]} ${da[Number(i) + 3]}</div>`;
-        iter += 1;
+  let da: Array<string> = await invoke("my_custom_command2");
+
+  const oldRows = document.querySelectorAll(".process-row");
+  oldRows.forEach(row => row.remove());
+
+  for (let i = 0; i < da.length; i += 5) {
+    const name = da[i];
+    const pid = da[i + 1];
+    const mem_use = da[i + 2];
+    const cpu_use = da[i + 3];
+    const gpu_use = da[i + 4];
+
+    const row = document.createElement("tr");
+    row.classList.add("process-row");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = selectedPIDs.has(pid);
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        selectedPIDs.add(pid);
+      } else {
+        selectedPIDs.delete(pid);
       }
-      else if (iter == 4) {
-        iter = 1;
-      }
-      else {
-        iter += 1;
-      }
-    }
+    });
+
+    row.innerHTML = `
+    <td class="pid">${pid}</td>
+    <td class="name">${name}</td>
+    <td class="mem_use">${mem_use}</td>
+    <td class="cpu_use">${cpu_use}</td>
+    <td class="gpu_use">${gpu_use}</td>`;
+    row.appendChild(checkbox);
+    table?.appendChild(row);
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  // greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#processes");
+document.addEventListener('DOMContentLoaded', () => {
   setInterval(greet, 1000);
   greet();
 });
